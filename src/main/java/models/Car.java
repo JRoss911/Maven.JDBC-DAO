42f955966ec99f50a7357ec767c82a9c938b7275
Jorden Ross <JRoss9117@gmail.com>
2024-04-23 11:56:33 -0400

    Made changes

---
+++ src/main/java/models/Car.java
@@ -0,0 +1,16 @@
+package models;
+
+public class Car {
+
+    private int id;
+    private String make;
+    private String model;
+    private Integer year;
+    private String color;
+
+    public Car(Integer id, String make, String model, Integer year, String brown) {
+    }
+
+
+    // constructors / standard setters / getters
+}
