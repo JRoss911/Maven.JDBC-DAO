09e2e2054e5646c7473707cd222b951186e98255
Jorden Ross <JRoss9117@gmail.com>
2024-04-23 11:04:56 -0400

    Made changes

---
+++ src/main/java/daos/Dao.java
@@ -0,0 +1,19 @@
+package daos;
+
+import org.junit.jupiter.params.shadow.com.univocity.parsers.annotations.Copy;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface Dao<T> {
+
+    Optional<T> findById(int id);
+
+    List<T> findAll();
+
+    void create(T t);
+
+    void update(T t, String[] params);
+
+    void delete(T t);
+}
